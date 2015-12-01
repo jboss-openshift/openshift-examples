b6eb287588205c7943cade23beb69b0712d0a9c5
William DeCoste <bdecoste@gmail.com>
2015-12-01 12:49:41 -0800

    CLOUD-411 example for custom settings.xml
    CLOUD-411 example for custom settings.xml
    
    CLOUD-411

---
+++ spring-eap6-quickstart-custom-configuration/src/main/java/org/jboss/tools/example/springmvc/mvc/MemberRestController.java
@@ -0,0 +1,32 @@
+package org.jboss.tools.example.springmvc.mvc;
+
+import java.util.List;
+
+import org.jboss.tools.example.springmvc.domain.Member;
+import org.jboss.tools.example.springmvc.repo.MemberDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/rest/members")
+public class MemberRestController
+{
+    @Autowired
+    private MemberDao memberDao;
+
+    @RequestMapping(method=RequestMethod.GET, produces="application/json")
+    public @ResponseBody List<Member> listAllMembers()
+    {
+        return memberDao.findAllOrderedByName();
+    }
+
+    @RequestMapping(value="/{id}", method=RequestMethod.GET, produces="application/json")
+    public @ResponseBody Member lookupMemberById(@PathVariable("id") Long id)
+    {
+        return memberDao.findById(id);
+    }
+}
